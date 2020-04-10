2ecadd386e2a622f0c55bcf320cb8d7687753953
vsonawane <vsonawane@vilas>
2020-04-10 17:46:31 +0530

    // linux = added copyright in class

--- AccountService/src/main/java/com/example/account/AccountController.java
+++ AccountService/src/main/java/com/example/account/AccountController.java
@@ -1,6 +1,7 @@
 package com.example.account;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
@@ -10,6 +11,11 @@ import org.springframework.web.client.RestTemplate;
 
 import com.example.conditional.Person;
 
+/**
+ * controller class for managing accoount (linux centos)
+ * @author vsonawane
+ *
+ */
 @RestController
 public class AccountController {
 	
